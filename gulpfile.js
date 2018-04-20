feec6325b41ff26e35b6f2d0aea6c63e507577b3
Hamish Gray <hamish.gray@secretescapes.com>
2018-04-20 18:30:51 +1200

    revisions to site. Added copy in. Removed sandals game

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,6 @@ gulp.task('build-main-js', function(cb) {
     './_assets/js/_components/offer-countdown.js',
     './_assets/js/_components/modal.js',
     './_assets/js/_components/sticky-nav.js',
-    './_assets/js/_components/game.js',
 
     // custom js for project
     './_assets/js/main.js',
